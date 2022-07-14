1ba1fd88d05d5b7180ae66f30d22122284958b8d
coderharshmalik <harshmalik20015@gmail.com>
2022-07-14 11:55:09 +0530

    final

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # from crypt import methods
 from re import TEMPLATE
 from flask import Flask, render_template, request
+from transliterate import TransliterateLanguage
 
 from indic_transliteration import sanscript
 from indic_transliteration.sanscript import transliterate
@@ -13,7 +14,8 @@ def getData():
     if request.method == "POST":
         if len(str(request.form["data"])):
             original_text = request.form["data"]
-            data = transliterate(original_text, sanscript.DEVANAGARI, sanscript.ITRANS)
+            language=request.form["language"]
+            data=TransliterateLanguage(language,original_text).convertTo()
         else:
             print("form is empty")
     return render_template('index.html', data=data)
--- transliterate.py
+++ transliterate.py
@@ -1,8 +1,21 @@
 from indic_transliteration import sanscript
 from indic_transliteration.sanscript import transliterate
 
-original_text='मख्खन लाल'
-
-print(transliterate(original_text, sanscript.DEVANAGARI ,sanscript.KOLKATA))
-
-#print(transliterate(original_text, sanscript.TELUGU ,sanscript.KOLKATA))
+class TransliterateLanguage:
+    def __init__(self, lanuage, original_text):
+        self.language=lanuage
+        self.languageSet = {
+            "Hindi": sanscript.DEVANAGARI,
+            "Gujarati": sanscript.GUJARATI,
+            "Kannada": sanscript.KANNADA,
+            "Bengali": sanscript.BENGALI,
+            #"Gurmukhi": sanscript.GURMUKHI,
+            "malayalam": sanscript.MALAYALAM,
+            "oriya": sanscript.ORIYA,
+            "tamil": sanscript.TAMIL,
+            "telugu": sanscript.TELUGU,
+        }
+        self.original_text=original_text
+    
+    def convertTo(self):
+        return transliterate(self.original_text, self.languageSet[self.language] ,sanscript.ITRANS) 
